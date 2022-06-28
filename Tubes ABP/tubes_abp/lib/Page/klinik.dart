dd52e0b04cd712ed575c067639baf595e5a7cf65
Muhammad Hikami Dzulfiqar <muhamadhikami48@gmail.com>
2022-06-28 22:03:28 +0700

    new

--- Tubes ABP/tubes_abp/lib/Page/klinik.dart
+++ Tubes ABP/tubes_abp/lib/Page/klinik.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
+import 'package:tubes_abp/Page/alamat.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../model/klinik_model.dart';
@@ -14,7 +15,9 @@ class Klinik extends StatefulWidget {
 }
 
 class _KlinikState extends State<Klinik> {
-  Stream<List<klinik>> getGroomings() => FirebaseFirestore.instance.collection("klinik").snapshots().map((event) => event.docs.map((e) => klinik.fromJson(e.data())).toList());
+  Stream<List<klinik>> getGroomings() =>
+      FirebaseFirestore.instance.collection("klinik").snapshots().map(
+          (event) => event.docs.map((e) => klinik.fromJson(e.data())).toList());
 
   @override
   Widget build(BuildContext context) {
@@ -33,26 +36,94 @@ class _KlinikState extends State<Klinik> {
           ),
         ),
         body: StreamBuilder<List<klinik>>(
-          stream: getGroomings(),
-          builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final groom = snapshot.data;
-            return ListView.builder(itemBuilder: (contect, index) {
-              return ListTile(
-                leading: Image(image: NetworkImage(groom![index].imageUrl!)),
-                title: Text(groom[index].name!),
-                // subtitle: Text("Deskripsi: ${groom[index].description}", maxLines: 3, overflow: TextOverflow.ellipsis,),
-        
-                subtitle: Text("Harga: ${groom[index].price}"),
+            stream: getGroomings(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final groom = snapshot.data;
+                return ListView.builder(
+                  itemBuilder: (contect, index) {
+                    return Container(
+                      child: Card(
+                        elevation: 5,
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 120,
+                              width: 120,
+                              child: Image(
+                                  image: NetworkImage(groom![index].imageUrl!),
+                                  fit: BoxFit.cover),
 
-        
-              );
-            }, itemCount: groom!.length,);
-          } else if (snapshot.hasError) {
-            return Text('Error');
-          } else {
-            return Center(child: CircularProgressIndicator(),);
-          }
-        }));
+                              // title: Text(groom[index].name!),
+                              // subtitle: Text("Harga: ${groom[index].price}"),
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    groom[index].name!,
+                                    style: blackTextStyle.copyWith(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    "Harga: ${groom[index].price}",
+                                    style: blackTextStyle.copyWith(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 16),
+                                    child: Text(
+                                      "${groom[index].description}",
+                                      style: blackTextStyle.copyWith(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 60, right: 16),
+                              child: ElevatedButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor:
+                                          Color.fromARGB(229, 233, 78, 39)),
+                                  child: const Text('Book'),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Alamat()),
+                                    );
+                                  }),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: groom!.length,
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error');
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 }
--- Tubes ABP/tubes_abp/lib/model/klinik_model.dart
+++ Tubes ABP/tubes_abp/lib/model/klinik_model.dart
@@ -2,33 +2,38 @@ class klinik {
   int? id;
   String? name;
   int? price;
+  String? description;
   String? imageUrl;
   Null? createdAt;
   Null? updatedAt;
 
-  klinik({this.id,
+  klinik(
+      {this.id,
       this.name,
       this.price,
+      this.description,
       this.imageUrl,
       this.createdAt,
       this.updatedAt});
 
   factory klinik.fromJson(dynamic json) {
     return klinik(
-      id : json['id'],
-    name : json['name'],
-    price : json['price'],
-    imageUrl : json['image_url'],
-    createdAt : json['created_at'],
-    updatedAt : json['updated_at']);
+        id: json['id'],
+        name: json['name'],
+        price: json['price'],
+        description: json['description'],
+        imageUrl: json['image_url'],
+        createdAt: json['created_at'],
+        updatedAt: json['updated_at']);
   }
 
   Map<String, dynamic> toJson() => {
-    'id' : id,
-    'name' : name,
-    'price' : price,
-    'image_url' : imageUrl,
-    'created_at' : createdAt,
-    'updated_at' : updatedAt,
-  };
+        'id': id,
+        'name': name,
+        'price': price,
+        'description': description,
+        'image_url': imageUrl,
+        'created_at': createdAt,
+        'updated_at': updatedAt,
+      };
 }
